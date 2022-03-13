476774794c10ada39b305e80731bc62405c0728f
J-Lik <popovkv75@gmail.com>
2022-03-13 21:53:40 +0500

    Внесение класса,переменной,вункции

---
+++ LessonOn/Constans.swift
@@ -0,0 +1,14 @@
+//
+// Created by Home on 13.03.2022.
+//
+
+import Foundation
+
+class Constans {
+
+    static let oneConastans = 10
+
+    static func tuCostans() {
+        print("I m good man")
+    }
+}
